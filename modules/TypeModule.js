28bc404e4a10547c232c83f750ebff201006f9b4
Shubhankar Dimri <dimrishubhi@gmail.com>
2019-09-13 19:19:31 +0530

    Making Grid Element

---
+++ modules/TypeModule.js
@@ -0,0 +1,15 @@
+
+export class GridElement {
+    constructor(value, canMerge) {
+        this.value = value;
+        this.canMerge = canMerge;
+    }
+    
+    // set value(value) {
+    //     this.value = value;
+    // }
+
+    // set canMerger(canMerge) {
+    //     this.canMerge = canMerge; 
+    // }
+}
