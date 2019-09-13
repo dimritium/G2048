7d3d4c7c5df0380f3c2c5baa8e8befec098521cd
Shubhankar Dimri <dimrishubhi@gmail.com>
2019-09-13 19:18:45 +0530

    Updated Right(not working) and up movement

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+import { GridElement } from "/modules/TypeModule.js";
+
+let gride = new GridElement(4, false);
+
 let grid = 4;
 let array = new Array(grid * grid);
 var arrayEmptyPos = new Array();
+
+for (let i = 0; i < grid * grid; i++) {
+    array[i] = new GridElement(0, true);
+}
+
+console.log(array[0].value);
 
 function gameEnv(grid) {
     let gameEnv = document.getElementById("game-env");
@@ -25,7 +35,7 @@ function getArrayEmptyPos() {
     // arrayEmptyPos.fill(undefined);
     arrayEmptyPos.length = 0;
     for (let i = 0; i < array.length; i++) {
-        if (array[i] == undefined) {
+        if (array[i].value == 0) {
             arrayEmptyPos.push(i);
         }
     }
@@ -34,21 +44,19 @@ function getArrayEmptyPos() {
         return false;
     }
     return true;
-    console.log(arrayEmptyPos, "emptyyyyyarrpos");
 }
 
 function randomInsert() {
     if (getArrayEmptyPos()) {
+        let val;
         let index = arrayEmptyPos[Math.floor(Math.random() * arrayEmptyPos.length)];
         let k = Math.floor(2 * Math.random());
         if (k == 0) val = 2;
         else val = 4;
-        // document.getElementById('val-' + (index + 1));
-        array[index] = val;
+        array[index].value = val;
         document
             .getElementById("val-" + (index + 1))
             .getElementsByTagName("div")[0].innerHTML = val;
-        // console.log(document.getElementById('val-' + (index + 1)).getElementsByTagName('div'));
     } else {
         alert("GameOver");
     }
@@ -60,7 +68,7 @@ function updateGridValue() {
             document
                 .getElementById("val-" + (i * grid + j + 1))
                 .getElementsByTagName("div")[0].innerHTML =
-                array[i * grid + j] == undefined ? "" : array[i * grid + j];
+                array[i * grid + j].value == 0 ? "" : array[i * grid + j].value;
         }
     }
     randomInsert();
@@ -69,32 +77,76 @@ function updateGridValue() {
 function handleMovements(event) {
     switch (event.key) {
         case "ArrowUp":
+            for (let row = 0; row < grid; row++) {
+                let flag = false;
+                for (let col = 0; col < grid - 1; col++) {
+                    for (let k = (col + 1) * grid + row; k > row; k -= 4) {
+                        if (array[k].value != array[k - grid].value && array[k - grid].value == 0) {
+                            array[k - grid] = array[k];
+                            array[k] = new GridElement(0, true);
+                        } else if (
+                            array[k].value == array[k - grid].value &&
+                            array[k].value != 0 &&
+                            !flag
+                        ) {
+                            array[k - grid] = new GridElement(array[k - grid].value * 2, true);
+                            array[k] = new GridElement(0, true);
+                            flag = true;
+                        }
+                    }
+                }
+            }
             break;
+
         case "ArrowDown":
             break;
+
         case "ArrowLeft":
-            console.log(array, "In <=");
             for (let row = 0; row < grid; row++) {
+                let flag = false;
                 for (let col = 0; col < grid - 1; col++) {
-                    let flag = false;
                     for (let k = row * grid + col + 1; k > row * grid; k--) {
-                        if (array[k] != array[k - 1] && array[k - 1] == undefined) {
+                        if (array[k].value != array[k - 1].value && array[k - 1].value == 0) {
                             array[k - 1] = array[k];
-                            array[k] = undefined;
-                        } else if (array[k] == array[k - 1] && array[k] != undefined && !flag) {
-                            array[k - 1] *= 2;
-                            array[k] = undefined;
+                            array[k] = new GridElement(0, true);
+                        } else if (
+                            array[k].value == array[k - 1].value &&
+                            array[k].value != 0 &&
+                            !flag
+                        ) {
+                            array[k - 1] = new GridElement(array[k - 1].value * 2, true);
+                            array[k] = new GridElement(0, true);
                             flag = true;
                         }
                     }
                 }
             }
             console.log(array);
-            updateGridValue();
             break;
+
         case "ArrowRight":
+            for (let row = 0; row < grid; row++) {
+                let flag = false;
+                for (let col = 0; col < grid - 1; col++) {
+                    for (let k = ((row + 1) * (grid - 1) - (col + 1)); k < ((row + 1) * grid - 1); k++) {
+                        if (array[k].value != array[k + 1].value && array[k + 1].value == 0) {
+                            array[k + 1] = array[k];
+                            array[k] = new GridElement(0, true);
+                        } else if (
+                            array[k].value == array[k + 1].value &&
+                            array[k].value != 0 &&
+                            !flag
+                        ) {
+                            array[k + 1] = new GridElement(array[k + 1].value * 2, true);
+                            array[k] = new GridElement(0, true);
+                            flag = true;
+                        }
+                    }
+                }
+            }
             break;
     }
+    updateGridValue();
     console.log(array);
     console.log(event.key);
 }
@@ -104,7 +156,9 @@ function main() {
     getArrayEmptyPos(array);
     randomInsert();
     document.addEventListener("keydown", function(event) {
-        handleMovements(event);
+        if (["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"].includes(event.key)) {
+            handleMovements(event);
+        }
     });
 }
 
